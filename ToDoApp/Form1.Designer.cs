de050d2b5a1657f40911e4debf9d6c9773b46883
ValeriyaMix <lerka-mishka@yandex.ru>
2022-08-16 21:43:23 +0300

    Add project files.

---
+++ ToDoApp/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ToDoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox_task = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_date = new System.Windows.Forms.TextBox();
+            this.textBox_category = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.task = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Complete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 355);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Postpone";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(666, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox_task
+            // 
+            this.textBox_task.Location = new System.Drawing.Point(44, 126);
+            this.textBox_task.Multiline = true;
+            this.textBox_task.Name = "textBox_task";
+            this.textBox_task.Size = new System.Drawing.Size(249, 110);
+            this.textBox_task.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Type your task";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.date,
+            this.task,
+            this.category});
+            this.dataGridView1.Location = new System.Drawing.Point(339, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(435, 248);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // textBox_date
+            // 
+            this.textBox_date.Location = new System.Drawing.Point(44, 53);
+            this.textBox_date.Name = "textBox_date";
+            this.textBox_date.Size = new System.Drawing.Size(249, 22);
+            this.textBox_date.TabIndex = 9;
+            // 
+            // textBox_category
+            // 
+            this.textBox_category.Location = new System.Drawing.Point(44, 279);
+            this.textBox_category.Name = "textBox_category";
+            this.textBox_category.Size = new System.Drawing.Size(249, 22);
+            this.textBox_category.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Task date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Task category";
+            // 
+            // date
+            // 
+            this.date.HeaderText = "Task date";
+            this.date.MinimumWidth = 6;
+            this.date.Name = "date";
+            this.date.Width = 125;
+            // 
+            // task
+            // 
+            this.task.HeaderText = "Task";
+            this.task.MinimumWidth = 6;
+            this.task.Name = "task";
+            this.task.Width = 125;
+            // 
+            // category
+            // 
+            this.category.HeaderText = "Task category";
+            this.category.MinimumWidth = 6;
+            this.category.Name = "category";
+            this.category.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_category);
+            this.Controls.Add(this.textBox_date);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_task);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox_task;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_date;
+        private System.Windows.Forms.TextBox textBox_category;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn task;
+        private System.Windows.Forms.DataGridViewTextBoxColumn category;
+    }
+}
+
---
+++ ToDoApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ToDoApp 
+{
+    public partial class Form1 : Form
+    {
+        SQLiteConnection con;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        string cs = @"URI=file:" + Application.StartupPath + "\\todo_database.db";
+        string path = "todo_database.db";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ShowData()
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            string stm = "SELECT * FROM TodoList";
+            var cmd = new SQLiteCommand(stm, con);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Insert(0, dr.GetString(1), dr.GetString(2), dr.GetString(3));
+            }
+        }
+
+        private void TableCreation()
+        {
+
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(cs))
+                {
+                    sqlite.Open();
+
+
+                    string sql = @"CREATE TABLE TodoList(id INTEGER PRIMARY KEY,
+            date TEXT, task TEXT, category TEXT, postponeTimes INT, isDeleted INT)";
+                    SQLiteCommand command = new SQLiteCommand(sql, sqlite);
+                    command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                //Console.WriteLine("Database cannot be created");
+            }
+            
+            
+        }
+            
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TableCreation();
+            ShowData();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+            var cmd = new SQLiteCommand(con);
+
+            try
+            {
+                cmd.CommandText = "INSERT INTO ToDoList(date, task, category) " +
+                "VALUES (@date, @task, @category)";
+
+                string DATE = textBox_date.Text;
+                string TASK = textBox_task.Text;
+                string CATEGORY = textBox_category.Text;
+
+                cmd.Parameters.AddWithValue("@date", DATE);
+                cmd.Parameters.AddWithValue("@task", TASK);
+                cmd.Parameters.AddWithValue("@category", CATEGORY);
+
+                dataGridView1.ColumnCount = 3;
+                dataGridView1.Columns[0].Name = "Task date";
+                dataGridView1.Columns[1].Name = "Task";
+                dataGridView1.Columns[2].Name = "Task category";
+
+                string[] row = new string[] { DATE, TASK, CATEGORY };
+
+                dataGridView1.Rows.Add(row);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("It is not possible to insert the data");
+
+            }
+        }
+    }
+}
